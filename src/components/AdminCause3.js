c8746ddc7f157c7c24393e0fbfaa2bbf92474da3
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-01-01 10:39:25 +0500

    Add files via upload

---
+++ src/components/AdminCause3.js
@@ -0,0 +1,89 @@
+import cause3 from './images/hxn.jpg'
+
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import userAddress from './userAddress';
+import {FUNDRAISER_ADDRESS, FUNDRAISER_ABI} from './config.js'
+import { Link, Switch, Route, BrowserRouter as Router} from 'react-router-dom';
+
+const AdminCause3= ({props}) => {
+  const [cause3Title, setCause3Title] = useState();
+  const [cause3Desc, setCause3Desc] = useState();
+
+  const [toAllocate, setToAllocate] = useState();
+
+  useEffect(() => {
+  async function load() {
+    const provider = new ethers.providers.JsonRpcProvider("http://localhost:7545")
+
+    const contract = new ethers.Contract(
+      FUNDRAISER_ADDRESS, FUNDRAISER_ABI, provider
+    )
+
+    const causeNames = await contract.getCauseNames();
+    setCause3Title(String(causeNames[2]));
+
+    const desc3 = String(await contract.getCauseDesc(cause3Title));
+    setCause3Desc(String(desc3));
+  }
+
+  load();
+  }, []);
+
+  function updateAmount(event) {
+  setToAllocate(event.target.value);
+  }
+
+  function allocateAmount(event) {
+  async function load() {
+    const provider = new ethers.providers.JsonRpcProvider("http://localhost:7545");
+    const signer = provider.getSigner(userAddress.value);
+
+    const contract = new ethers.Contract(
+      FUNDRAISER_ADDRESS, FUNDRAISER_ABI, signer
+    );
+
+    console.log(userAddress.value);
+    console.log(toAllocate);
+    const var_ = await contract.allocateGeneralFunds(cause3Title, ethers.utils.parseEther(toAllocate), {from:userAddress.value});
+  }
+
+  load();
+  }
+
+return (
+
+    <div className="donate">
+        <header className='header-cause'>
+            <img src={cause3} className='img-cause'/>
+                {cause3Title}
+            <p className='p-cause'>
+                {cause3Desc}
+            </p>
+            <p className='space'>
+                Empty space
+            </p>
+
+            <p className='target-amount'>
+                Enter the amount you want to allocate:
+            </p>
+
+            <div className="form">
+                <div className="form-group">
+                    <label htmlFor="amount">Amount</label>
+                    <input type="amount" name="amount" placeholder="amount" onChange={updateAmount}/>
+                </div>
+            </div>
+
+              <button type="button" className="donate-btn">
+                <Link to="/popup" onClick={allocateAmount}>
+                    Allocate
+                </Link>
+              </button>
+        </header>
+    </div>
+);
+}
+
+
+export default AdminCause3;
